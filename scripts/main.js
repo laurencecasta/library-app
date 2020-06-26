32b993507affdd41282b4df6509269c11717bbe5
Laurence <larrycasta11@gmail.com>
2020-06-26 12:36:51 -0500

    refactor constructors using class keyword syntax

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,19 @@ if (localStorage.getItem('myLibrary')) {
   myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
 }
 
-function Book(title, author, pages, bookRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.bookRead = bookRead;
-  this.info = () => {
-    return (bookRead) ? `${title} by ${author}, ${pages} pages, book was read.` :
-    `${title} by ${author}, ${pages} pages, not read yet.`;
+// Declare Book class to create Book objects
+class Book {
+  constructor(title, author, pages, bookRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.bookRead = bookRead;
   }
-}
+
+  info() {
+    console.log(`${this.title} by ${this.author}, ${this.pages} pages, ` + (this.bookRead ? 'book was read.' : 'not read yet.'));
+  }
+};
 
 function addBookToLibrary (title, author, pages, bookRead) {
   let newBook = new Book(title, author, pages, bookRead);
